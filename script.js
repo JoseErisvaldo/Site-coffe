d28e765dbb50042163c0a16175981b25ff1bc016
JoseErisvaldo <100242848+JoseErisvaldo@users.noreply.github.com>
2023-01-28 16:17:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+let selecionar = document.getElementById('nav-button')
+
+selecionar.addEventListener('click', function c () {
+    let mudar = document.querySelector('.barra-nav')
+
+    mudar.style.display = 'flex'
+})
+
